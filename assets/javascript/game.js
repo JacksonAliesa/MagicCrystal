dc58ed05f815162f02f0e8dabd30bfb2738adeb7
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-10-21 23:04:47 -0400

    got the crystal buttons functioning...the user score is now dislayed after each click function, new to figure out how to reset after win/loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,7 @@ window.onload;
 {
 	//could I make this a function to reset the game after win?
 	// initialize random number for 19-120
-	// chooses the random # between 19-120
-	// display this in div tag for the displahy on html page for magic crystal
+	// display this in div tag for the display on html page for magic crystal
 
 	var randomNum = Math.floor(Math.random() * 120 + 19);
 	// displays the randomized number on html file
@@ -34,28 +33,35 @@ window.onload;
 	// need to add the click capability to each crystal
 	// add jquery to grab html to display
 	//function is triggered when button is clicked
+	//update & display userscore when clicked
 	$('#btnPink').on('click', function() {
-        $(this).val();
-        console.log(btnPink + userScore);
+		userScore = userScore + btnPink;
+        $('#displayScore').text(userScore);
         
-        
+        if(userScore === randomNum){
+            wins++;
+        }
+        else{
+            losses--;
+        }
 	});
 
-	$("#btnOrange").on("click", function() {
-	    $(this).val();
-        console.log(btnOrange + userScore);
+	$('#btnOrange').on('click', function() {
+		userScore = userScore + btnOrange;
+		$('#displayScore').text(userScore);
 	});
 
-	$("#btnGreen").on("click", function() {
-	    $(this).val();
-        console.log(btnGreen + userScore);
+	$('#btnGreen').on('click', function() {
+		userScore = userScore + btnGreen;
+		$('#displayScore').text(userScore);
 	});
 
-	$("#btnBlue").on("click", function() {
-	    $(this).val();
-        console.log(btnBlue + userScore);
+	$('#btnBlue').on('click', function() {
+		userScore = userScore + btnBlue;
+		$('#displayScore').text(userScore);
 	});
 
-	//test & debug
-	// console.log(userScore);
+	//player whens if score matches randomnumber aka magic crystal
+	//player loses if their score exceeds randomnumber aka magic crystal
+	//game should reset once the player wins or loses
 }
