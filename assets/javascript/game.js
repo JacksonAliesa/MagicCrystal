267b75683c35a8612a4592b17b9d98f3c097ed04
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-10-21 20:08:02 -0400

    console loggeed the clik function for each crystal using this method, now need to figure out how to display it on html for score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,61 @@
-window.onload; {
-    // initialize random number for 19-120
-    // chooses the random # between 19-120
-    // display this in div tag for 
-var randomNum = Math.floor((Math.random() * 120) + 19);
+window.onload;
+{
+	//could I make this a function to reset the game after win?
+	// initialize random number for 19-120
+	// chooses the random # between 19-120
+	// display this in div tag for the displahy on html page for magic crystal
 
-$("#displayCrystal").html("<h2>Magic Crystal<h2>" + randomNum);
+	var randomNum = Math.floor(Math.random() * 120 + 19);
+	// displays the randomized number on html file
+	$('#displayCrystal').html('<h2>Magic Crystal<h2>' + randomNum);
 
+	//variables for the randomized # for each color block
+	var btnPink = Math.floor(Math.random() * 12 + 1);
+	var btnOrange = Math.floor(Math.random() * 12 + 1);
+	var btnGreen = Math.floor(Math.random() * 12 + 1);
+	var btnBlue = Math.floor(Math.random() * 12 + 1);
 
-// variables to display the wins/losses/totalscore of user
-var wins = 0;
-var losses = 0;
-var totalScore = 0;
+	//test and debug -- worked and showed randomized numbering
+	console.log(btnPink);
+	console.log(btnOrange);
+	console.log(btnGreen);
+	console.log(btnBlue);
 
-//variables for the randomized # for each color block
-var btnPink = Math.floor((Math.random() * 12) + 1);
-var btnOrange = Math.floor((Math.random() * 12) + 1);
-var btnGreen = Math.floor((Math.random() * 12) + 1);
-var btnBlue = Math.floor((Math.random() * 12) + 1);
+	// variables to display the wins/losses/totalscore of user
+	var wins = 0;
+	var losses = 0;
+	var userScore = 0;
 
-// need to add the click capability to each crystal
-$("#btnPink").on("click", function() {
-  
-}
+	//display wins/losses on html
+	$('#winDisplay').html('<h4>Wins:<h4>' + wins);
+	$('#lossDisplay').html('<h4>Loss:<h4>' + losses);
+	$('#displayScore').html('<h4>Your Score Is:<h4>' + userScore);
 
-$("#btnOrange").on("click", function() {
-  
-}
+	// need to add the click capability to each crystal
+	// add jquery to grab html to display
+	//function is triggered when button is clicked
+	$('#btnPink').on('click', function() {
+        $(this).val();
+        console.log(btnPink + userScore);
+        
+        
+	});
 
-$("#btnGreen").on("click", function() {
-  
-}
+	$("#btnOrange").on("click", function() {
+	    $(this).val();
+        console.log(btnOrange + userScore);
+	});
 
-$("#btnBlue").on("click", function() {
-  
-}
+	$("#btnGreen").on("click", function() {
+	    $(this).val();
+        console.log(btnGreen + userScore);
+	});
 
+	$("#btnBlue").on("click", function() {
+	    $(this).val();
+        console.log(btnBlue + userScore);
+	});
 
-
-
-
-
-
-
+	//test & debug
+	// console.log(userScore);
 }
